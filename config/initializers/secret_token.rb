ac58241b7a211d9d9602035a998de6777225553e
Rebelition <rebelition@rebelition-MS-16G1.(none)>
2013-12-24 17:10:00 +0700

    add project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '67d557a97c9fd797236b8cf47dca7a9d20f93203ca4c758e7d31c2b9e0a1f6d86d8b5965a54cb471627dce8ffc139a9920ca277afd7fa4fb9dceb259fdec9e05'
